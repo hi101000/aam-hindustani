d16c9a38a15bef4453cdf9f04c1d3ddc362e6a6a
hi101000 <60079731+hi101000@users.noreply.github.com>
2026-02-23 14:13:21 -0500

    basic setup

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+const port = 3000;
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Aam Hindustani app listening at http://localhost:${port}`);
+});
