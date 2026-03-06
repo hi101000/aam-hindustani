d8d4134cf5525de8320eb7421ce71ba3e0ad4e56
hi101000 <60079731+hi101000@users.noreply.github.com>
2026-03-05 20:22:32 -0500

    Got Redis working, I think

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const path = require('path');
+const { Redis } = require("@upstash/redis");
+
+require('dotenv').config({ path: './.env.local' });
+
+const redis = Redis.fromEnv();
 
 const app = express();
 const port = 3000;
@@ -14,6 +19,16 @@ nunjucks.configure('views', {
 
 app.get('/', (req, res) => {
     res.render('index.njk');
+});
+
+app.get('/test', async (req, res) => {
+    try {
+        const result = await redis.get("Articles");
+        res.send(`Value from Redis: ${result}`);
+    } catch (error) {
+        console.error('Error interacting with Redis:', error);
+        res.status(500).send('Error interacting with Redis');
+    }
 });
 
 app.listen(port, () => {
